bac629714112e890ecdb4b417ee08875461ff8de
Dinçer Topal <rnytpl@gmail.com>
2023-03-20 18:59:36 +0300

    Some more fixes

--- src/App.js
+++ src/App.js
@@ -11,34 +11,29 @@ import HomePage from "scenes/HomePage/HomePage";
 import UserWidget from "scenes/Widgets/UserWidget";
 import ProfilePage from "scenes/ProfilePage/ProfilePage";
 import Prefetch from "features/auth/Prefetch";
+import Layout from "components/Layout";
 
 const App = () => {
+  const { mode, user } = useSelector((state) => state.auth);
+
   // Pass selected mode as an argument to themeSettings so that createTheme can generate a theme with given options
   // After that useMemo is going to memoize and cache results returned from createTheme and only run again when the dependency has altered
-  const { mode, user } = useSelector((state) => state.auth);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
-    <Box className="app" width="100vw">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Navbar />
-        <Routes>
-          <Route element={<Prefetch />}>
-            <Route path="/home" element={user ? <HomePage /> : <LoginPage />} />
-            <Route
-              path="/:id"
-              element={user ? <UserWidget /> : <LoginPage />}
-            />
-            <Route path="/login" element={<LoginPage />} />
-            <Route
-              path="/profile/:id"
-              element={user ? <ProfilePage /> : <LoginPage />}
-            />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    </Box>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route element={<Prefetch />}>
+          <Route path="/home" element={user ? <HomePage /> : <LoginPage />} />
+          <Route path="/:id" element={user ? <UserWidget /> : <LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/profile/:id"
+            element={user ? <ProfilePage /> : <LoginPage />}
+          />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
 export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import Navbar from "scenes/Navbar/Navbar";
+import { themeSettings } from "theme";
+
+const Layout = () => {
+  const { mode, user } = useSelector((state) => state.auth);
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <Box className="app" width="100vw">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Outlet />
+      </ThemeProvider>
+    </Box>
+  );
+};
+export default Layout;
