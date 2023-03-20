cb81f2154d205e7eca205fd89ecec8fc37b3cd56
Dinçer Topal <rnytpl@gmail.com>
2023-03-20 18:18:00 +0300

    small fix

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL,
+    baseUrl: "https://sociopedia-api-dso2.onrender.com",
     prepareHeaders: (headers, { getState }) => {
       // getState allows you to access your redux store
       const token = getState().auth.token;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./store/store";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 root.render(
   <React.StrictMode>
