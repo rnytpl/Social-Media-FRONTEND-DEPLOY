2f2e5708400f34e9dc2176b74ce05d249c9d56dd
Dinçer Topal <rnytpl@gmail.com>
2023-03-20 20:01:54 +0300

    User Image fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./scenes/Navbar/Navbar";
 import LoginPage from "./scenes/LoginPage/LoginPage";
 import HomePage from "scenes/HomePage/HomePage";
 import UserWidget from "scenes/Widgets/UserWidget";
@@ -14,11 +9,10 @@ import Prefetch from "features/auth/Prefetch";
 import Layout from "components/Layout";
 
 const App = () => {
-  const { mode, user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
 
   // Pass selected mode as an argument to themeSettings so that createTheme can generate a theme with given options
   // After that useMemo is going to memoize and cache results returned from createTheme and only run again when the dependency has altered
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <Routes>
--- src/components/CommentUserImage.jsx
+++ src/components/CommentUserImage.jsx
@@ -1,12 +1,17 @@
 import { Box } from "@mui/material";
-import { selectUserById } from "features/users/usersApiSlice";
+import { selectUserById, useGetUsersQuery } from "features/users/usersApiSlice";
 import { useSelector } from "react-redux";
 
 const CommentUserImage = ({ image, userId, size = "25px" }) => {
   console.log(userId, "userId CommentUserImage");
-  const commentOwner = useSelector((state) => selectUserById(state, userId));
+  // const commentOwner = useSelector((state) => selectUserById(state, userId));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data.entities[userId],
+    }),
+  });
 
-  console.log(commentOwner, "commentOwner");
+  console.log(user, "commentOwner");
   return (
     <Box width={size} height={size} mb="0.5rem">
       <img
@@ -14,7 +19,7 @@ const CommentUserImage = ({ image, userId, size = "25px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={commentOwner.picturePath}
+        src={user.picturePath}
       />
     </Box>
   );
